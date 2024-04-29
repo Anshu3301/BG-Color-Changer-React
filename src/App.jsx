61a0ac039a4d5949b9541e6aed4f5da8c7e40145
Anshu Sain <sainanshu40@gmail.com>
2024-04-30 01:26:25 +0530

    add files

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+
+export default function App() {
+  const [color, setColor] = useState("gray")
+
+  return (
+    <div className="h-screen w-screen " style={{backgroundColor:color}}>
+      <div className="h-12 w-4/5 bg-orange-400 fixed bottom-10 left-[10%] flex items-center justify-around rounded-xl">
+            <button className="h-10 w-12 rounded-xl text-center bg-red-600 text-white " onClick={()=> setColor("red")}>Red</button>
+            <button className="h-10 w-12 rounded-xl text-center bg-green-600 text-white " onClick={()=> setColor("green")}>Green</button>
+            <button className="h-10 w-12 rounded-xl text-center bg-blue-600 text-white " onClick={()=> setColor("blue")}>Blue</button>
+            <button className="h-10 w-12 rounded-xl text-center bg-yellow-500 text-white " onClick={()=> setColor("yellow")}>Yellow</button>
+            <button className="h-10 w-12 rounded-xl text-center bg-pink-600 text-white " onClick={()=> setColor("pink")}>Pink</button>
+            <button className="h-10 w-12 rounded-xl text-center bg-purple-600 text-white " onClick={()=> setColor("purple")}>Purple</button>
+            <button className="h-10 w-12 rounded-xl text-center bg-teal-600 text-white " onClick={()=> setColor("teal")}>Teal</button>
+      </div>
+    </div>
+  )
+}
+
